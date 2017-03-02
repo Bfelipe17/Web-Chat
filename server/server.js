c3d149a8fc2cb5dd9efff159346cb21333d4d053
Bruno Felipe <bruno.felipe7170@gmail.com>
2017-03-02 12:23:21 -0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket) {
   console.log('New user connected');
 
+  socket.on('createMessage', function(message) {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect', function() {
     console.log('User was disconnected');
   })
